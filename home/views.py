5a0b7e1403387971efa1fe2af9e1e3da8a68c3a6
Sudish Karki <sudishkarki360@gmail.com>
2025-06-27 16:04:28 +0545

    ad all code

--- home/views.py
+++ home/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hyy, Its me Sudish Karki")
+    context = {
+        "variable1": "This is sent",
+        "variable2": "Sudish Karki"
+    }
+    return render(request, 'index.html', context)
+    # return HttpResponse("Hyy, Its me Sudish Karki")
 
 def about(request):
     return HttpResponse("Inaruwa-2, Sunsari")
